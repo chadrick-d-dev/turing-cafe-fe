3ca096a46f966c641db68d7616163f45db9e88e8
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-16 12:11:16 -0700

    Write and pass tests for ResForm

--- src/ResForm/ResForm.js
+++ src/ResForm/ResForm.js
@@ -3,10 +3,14 @@ import React from 'react';
 const ResForm = (props) => {
   return (
     <form className='reservation-form'>
-      <input name='name' type='text' value={props.name} onChange={props.handleChange}></input>
-      <input name='date' type='text' value={props.date} onChange={props.handleChange}></input>
-      <input name='time' type='text' value={props.time} onChange={props.handleChange}></input>
-      <input name='number' type='text' value={props.number} onChange={props.handleChange}></input>
+      <label for='name-input'>Name</label>
+      <input id='name-input' name='name' type='text' value={props.name} onChange={props.handleChange}></input>
+      <label for='date-input'>Date</label>
+      <input id='date-input' name='date' type='text' value={props.date} onChange={props.handleChange}></input>
+      <label for='time-input'>Time</label>
+      <input id='time-input' name='time' type='text' value={props.time} onChange={props.handleChange}></input>
+      <label for='number-input'>Number</label>
+      <input id='number-input' name='number' type='text' value={props.number} onChange={props.handleChange}></input>
       <button name='submitButton' onClick={props.submitReservation}>Make Reservation</button>
     </form>
   )
--- src/ResForm/ResForm.test.js
+++ src/ResForm/ResForm.test.js
@@ -1,11 +1,48 @@
 import {render, screen, fireEvent, waitFor } from '@testing-library/react';
-import 'testing-library/jest-dom';
+import '@testing-library/jest-dom';
+import React from 'react';
 import ResForm from './ResForm';
 
 describe('ResForm', () => {
+  let mockHandleChange;
+  let mockSubmitReservation;
+  beforeEach(() => {
+    mockHandleChange = jest.fn();
+    mockSubmitReservation = jest.fn();
+  })
   it('should render form', () => {
     render(
-      <ResForm />
+      <ResForm 
+        handleChange= {mockHandleChange}
+        name=""
+        date=""
+        time=""
+        number=""
+        submitReservation={mockSubmitReservation}
+      />
     )
+
+      const submitButton = screen.getByText('Make Reservation');
+
+      expect(submitButton).toBeInTheDocument();
   })
+
+  it('should update input value props on change', () => {
+    render(
+      <ResForm 
+        handleChange= {mockHandleChange}
+        name=""
+        date=""
+        time=""
+        number=""
+        submitReservation={mockSubmitReservation}
+      />
+    )
+    const nameInput = screen.getByLabelText('Name');
+
+    fireEvent.change(nameInput, { target: { value: 'Chad' } })
+
+    expect(mockHandleChange).toHaveBeenCalledTimes(1);
+  })
+
 })
